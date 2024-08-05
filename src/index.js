3df30ff413953034bb1e9ab75bf8ba9fbf078d2e
apkoha <79258179+apkoha@users.noreply.github.com>
2024-08-05 12:15:35 +0300

    feat: reverse function written

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    let negativeNumber = String(n).slice(1).split("").reverse().join("");
+    return Number(negativeNumber);
+  } else {
+    let reverseArgument = String(n).split("").reverse().join("");
+    return Number(reverseArgument);
+  }
 }
